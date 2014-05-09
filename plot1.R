0dbed23e098864df953efc8721e63553f7b8a3e5
ksrinath <k.srinath@gmail.com>
2014-05-09 09:41:04 +0530

    code cleanup

--- plot1.R
+++ plot1.R
@@ -1,4 +1,3 @@
-library('data.table')
 start <- 66638
 end <- 69517
 nrows <- end - start
@@ -7,7 +6,7 @@ output <- "plot1.png"
 data <- read.csv('household_power_consumption.txt', skip = 66638, 
                  nrows = nrows, na.strings = "?", sep = ";")
 
-setnames(data,c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+colnames(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
 png(file = output)
 hist(data$Global_active_power, main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ output <- "plot2.png"
 data <- read.csv('household_power_consumption.txt', skip = 66638, 
               nrows = nrows, na.strings = "?", sep = ";")
 
-setnames(data,c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+colnames(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 data$DateTime <- strptime(paste(as.Date(data$Date, "%d/%m/%Y"), data$Time), "%Y-%m-%d %H:%M:%S")
 
 png(file = output)
